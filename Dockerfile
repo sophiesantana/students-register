791bc0727178b7e4299541f85746ac21941d09cd
Sophie Santana <ssophiesantana@gmail.com>
2024-08-04 23:08:09 -0300

    feat: insere configuracoes pro swagger

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ COPY . .
 
 EXPOSE 3000
 
-ENTRYPOINT [ "npm", "run" ]
+ENTRYPOINT [ "npm", "run"]
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function apiStudents() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function apiStudents() {
     allowedHeaders: 'Content-Type, Accept',
     credentials: true
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 apiStudents();
