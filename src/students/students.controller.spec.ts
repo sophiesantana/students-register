dc925040a6c4e708da59805166522d44ca57800b
Sophie Santana <ssophiesantana@gmail.com>
2024-08-05 00:56:34 -0300

    test: controller - criar usuario

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -1,20 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { CreateStudentDto } from './dto/create-student.dto';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let service: StudentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [
+        {
+          provide: StudentsService,
+          useValue: {
+            create: jest.fn()
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
+    service = module.get<StudentsService>(StudentsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a student', async () => {
+      const createStudentDto: CreateStudentDto = {
+        name: 'Teste 1',
+        cpf: '12345678910',
+        email: 'teste@teste.com',
+      };
+
+      const result = { id: 'uuid', ...createStudentDto };
+      jest.spyOn(service, 'create').mockResolvedValue(result as any);
+
+      expect(await controller.create(createStudentDto)).toEqual(result);
+    });
   });
 });
--- src/students/students.service.spec.ts
+++ src/students/students.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsService } from './students.service';
+import { Repository } from 'typeorm';
+import { Student } from './entities/student.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('StudentsService', () => {
   let service: StudentsService;
+  let repository: Repository<Student>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StudentsService],
+      providers: [
+        StudentsService,
+        {
+          provide: getRepositoryToken(Student),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<StudentsService>(StudentsService);
+    repository = module.get<Repository<Student>>(getRepositoryToken(Student));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new student', async () => {
+      const createStudentDto = {
+        name: 'Teste 1',
+        cpf: '12345678910',
+        email: 'teste@teste.com',
+      };
+
+      const savedStudent = {
+        ...createStudentDto,
+        id: 'uuid',
+      };
+
+      jest.spyOn(repository, 'save').mockResolvedValue(savedStudent as any);
+
+      expect(await service.create(createStudentDto)).toEqual(savedStudent);
+    });
   });
 });
