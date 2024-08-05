e9cecba7c9c46c3c05eed816ecb90525380cc019
Sophie Santana <ssophiesantana@gmail.com>
2024-08-05 01:02:42 -0300

    test: controller - finaliza testes

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
+import { NotFoundException } from '@nestjs/common';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
@@ -14,7 +16,11 @@ describe('StudentsController', () => {
         {
           provide: StudentsService,
           useValue: {
-            create: jest.fn()
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
           },
         },
       ],
@@ -40,6 +46,53 @@ describe('StudentsController', () => {
       jest.spyOn(service, 'create').mockResolvedValue(result as any);
 
       expect(await controller.create(createStudentDto)).toEqual(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of students', async () => {
+      const result = [{ id: 'uuid', name: 'Teste 1' }];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result as any);
+
+      expect(await controller.findAll({})).toEqual(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a student by id', async () => {
+      const result = { id: 'uuid', name: 'John Doe' };
+      jest.spyOn(service, 'findOne').mockResolvedValue(result as any);
+
+      expect(await controller.findOne('uuid')).toEqual(result);
+    });
+
+    it('should throw NotFoundException if student not found', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(null);
+
+      try {
+        await controller.findOne('uuid');
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('update', () => {
+    it('should update a student', async () => {
+      const updateStudentDto: UpdateStudentDto = { name: 'Jane Doe' };
+      const result = { id: 'uuid', name: 'Jane Doe' };
+      jest.spyOn(service, 'update').mockResolvedValue(result as any);
+
+      expect(await controller.update('uuid', updateStudentDto)).toEqual(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a student', async () => {
+      const result = { affected: 1 };
+      jest.spyOn(service, 'remove').mockResolvedValue(result as any);
+
+      expect(await controller.remove('uuid')).toEqual(result);
     });
   });
 });
